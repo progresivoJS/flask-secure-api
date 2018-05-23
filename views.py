dcced178f2cab71b626574e40ab096c46a8de186
progresivoJS <progresivoJS@gmail.com>
2018-05-24 02:01:41 +0900

    feat: implement protected resource api using flask_httpauth

--- views.py
+++ views.py
@@ -1,8 +1,11 @@
 from models import Base, User
-from flask import Flask, jsonify, request, url_for, abort
+from flask import Flask, jsonify, request, url_for, abort, g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
 from flask import Flask
 
@@ -12,6 +15,14 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 app = Flask(__name__)
+
+@auth.verify_password
+def verify_password(username, password):
+    user = session.query(User).filter_by(username = username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
 
 @app.route('/api/users', methods = ['POST'])
 def new_user():
@@ -34,6 +45,12 @@ def get_user(id):
     if not user:
         abort(400)
     return jsonify({'username' : user.username})
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    print(request.headers)
+    return jsonify({'data' : 'Hello, {}'.format(g.user.username)})
 
 if __name__ == '__main__':
     app.debug = True
